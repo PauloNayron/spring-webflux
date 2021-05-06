ffe4fff1cd78150c921a52e3bbad58be88e069f6
PauloNayron <paulonayron.s@hotmail.com>
2021-05-05 22:45:50 -0300

    feat: adiciona configuração do blockhout para não bloquear nenhuma thread

--- src/main/java/com/example/springwebflux/SpringWebfluxApplication.java
+++ src/main/java/com/example/springwebflux/SpringWebfluxApplication.java
@@ -2,9 +2,13 @@ package com.example.springwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxApplication {
+    static {
+        BlockHound.install();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebfluxApplication.class, args);
